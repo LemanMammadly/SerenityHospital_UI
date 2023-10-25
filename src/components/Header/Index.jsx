6563fcc4924bcf28084fcf1cd8bd940000236d7c
imac <leman.mammadly23@gmail.com>
2023-10-25 16:16:45 +0400

    axios data

--- src/components/Header/Index.jsx
+++ src/components/Header/Index.jsx
@@ -1,68 +1,124 @@
-import React from 'react'
-import './Index.css'
-import { Link } from 'react-router-dom'
-import $ from "jquery"
-import Logo from "../../assets/imgs/logo-no-background.png"
+import React, { useEffect, useState } from "react";
+import "./Index.css";
+import { Link } from "react-router-dom";
+import $ from "jquery";
+import axios from "axios";
 
 const Index = () => {
+  const [data, setData] = useState([]);
 
-    const respmenu = () => {
-        const containerResp = $('.container-resp');
-        
-        containerResp.fadeIn('slow', () => {
-        });
-    }
-
-    const closeRespMenu = () => {
-        const containerResp = $('.container-resp');
-        
-        containerResp.fadeOut('slow', () => {
-        });
-    }
+  useEffect(() => {
+    axios
+      .get("https://localhost:7227/api/Settings")
+      .then((resp) => {
+        setData(resp.data);
+      })
+      .catch((error) => {
+        console.log("error");
+      });
+  }, []);
 
 
-    const closeMenu=()=>{
-        const containerResp = $('.container-resp');
-        
-        containerResp.fadeOut('slow', () => {
-        });
-    }
+  const respmenu = () => {
+    const containerResp = $(".container-resp");
+
+    containerResp.fadeIn("slow", () => {});
+  };
+
+  const closeRespMenu = () => {
+    const containerResp = $(".container-resp");
+
+    containerResp.fadeOut("slow", () => {});
+  };
+
+  const closeMenu = () => {
+    const containerResp = $(".container-resp");
+
+    containerResp.fadeOut("slow", () => {});
+  };
 
   return (
     <header>
-        <div className="container">
+      <div className="container">
         <div className="left-logo">
-            <Link to='/'><img src={Logo} style={{width:"40%"}} alt="" /></Link>
+          {data.map((datas, index) => (
+            <Link key={index} to="/">
+              <img
+               src={datas.headerLogoUrl}
+                alt="wewere"
+                style={{ width: "40%" }}
+              />
+            </Link>
+          ))}
         </div>
         <div className="right">
-            <ul>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/doctors'>Doctors</Link></li>
-                <li><Link to='/appoinment'>Appoinment</Link></li>
-                <li><Link to='/aboutus'>About</Link></li>
-                <li><Link to='/contact'>Contact</Link></li>
-                <li><Link to='/login'>Login</Link></li>
-            </ul>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/doctors">Doctors</Link>
+            </li>
+            <li>
+              <Link to="/appoinment">Appoinment</Link>
+            </li>
+            <li>
+              <Link to="/aboutus">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
         </div>
         <div className="resp-icon">
-            <i onClick={respmenu} className="fa-solid fa-bars"></i>
+          <i onClick={respmenu} className="fa-solid fa-bars"></i>
         </div>
-        </div>
-        <div className="container-resp">
+      </div>
+      <div className="container-resp">
         <div className="resp-menu">
-          <i onClick={closeRespMenu} className="fa-solid fa-xmark resp-icon-close"></i>
-            <ul>
-                <li><Link onClick={closeMenu} to='/'>Home</Link></li>
-                <li><Link onClick={closeMenu} to='/doctors'>Doctors</Link></li>
-                <li><Link onClick={closeMenu} to='/apoinment'>Appoinment</Link></li>
-                <li><Link onClick={closeMenu} to='/aboutus'>About</Link></li>
-                <li><Link onClick={closeMenu} to='/contact'>Contact</Link></li>
-                <li><Link onClick={closeMenu} to='/login'>Login</Link></li>
-            </ul>
+          <i
+            onClick={closeRespMenu}
+            className="fa-solid fa-xmark resp-icon-close"
+          ></i>
+          <ul>
+            <li>
+              <Link onClick={closeMenu} to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link onClick={closeMenu} to="/doctors">
+                Doctors
+              </Link>
+            </li>
+            <li>
+              <Link onClick={closeMenu} to="/apoinment">
+                Appoinment
+              </Link>
+            </li>
+            <li>
+              <Link onClick={closeMenu} to="/aboutus">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link onClick={closeMenu} to="/contact">
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link onClick={closeMenu} to="/login">
+                Login
+              </Link>
+            </li>
+          </ul>
         </div>
-        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
